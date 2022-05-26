def11600fef45173a2b772c26c22bda4eb006bc7
aggarver <aggarver@sheridancollege.ca>
2022-05-26 06:33:31 -0700

    week 3 example

---
+++ src/printingstudentdetails/Student.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printingstudentdetails;
+
+/**
+ *
+ * @author verni
+ */
+class Student {
+    private String name;
+    private int age;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the num
+     */
+    public int getAge() {
+        return age;
+    
+}
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
